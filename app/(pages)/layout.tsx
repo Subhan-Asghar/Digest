132c5626d696d9e7a62c0f07f1069e2a6d2911bb
Subhan-Asghar <subhanasghar16@gmail.com>
2026-07-24 10:57:00 -0700

    Select Document

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,23 +3,39 @@ import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Navbar from "@/components/navbar";
-export default function PageLayout({
+
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { Spinner } from "@/components/ui/spinner";
+export default async function PageLayout({
 
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+         return (
+            <div className='flex h-full w-full items-center justify-center '>
+                <Spinner />
+            </div>
+        )
+    }
     return (
         <TooltipProvider delayDuration={0}>
-        <SidebarProvider>
-            <AppSidebar />
-            <div className="flex flex-1 flex-col h-screen overflow-hidden">
-           <Navbar />
-          <main className="flex-1 overflow-hidden">
-            {children}
-          </main>
-        </div>
-        </SidebarProvider>
+            <SidebarProvider>
+                <AppSidebar user={session.user} />
+                <div className="flex flex-1 flex-col min-h-screen w-full">
+                    <Navbar />
+                    <main className="flex-1 overflow-y-auto ">
+                        {children}
+                    </main>
+                </div>
+            </SidebarProvider>
         </TooltipProvider>
     )
 
